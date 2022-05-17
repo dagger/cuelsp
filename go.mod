cc026f4a538ae61f88bd5ebdec9f5790ee417679
guillaume <guillaume.derouville@gmail.com>
2022-05-17 17:29:31 +0200

    Add log debug
    To check if handshake happens
    
    Signed-off-by: guillaume <guillaume.derouville@gmail.com>

--- go.mod
+++ go.mod
@@ -3,14 +3,21 @@ module github.com/dagger/dlsp
 go 1.17
 
 require (
-	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/beevik/etree v1.1.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
+	github.com/gdexlab/go-render v1.0.1 // indirect
+	github.com/goccy/go-yaml v1.9.5 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/kortschak/utter v1.5.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
@@ -26,12 +33,15 @@ require (
 	github.com/spf13/viper v1.6.3 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/tliron/glsp v0.1.1 // indirect
-	github.com/tliron/kutil v0.1.56 // indirect
+	github.com/tliron/kutil v0.1.59 // indirect
+	github.com/tliron/yamlkeys v1.3.5 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
 	github.com/zchee/color/v2 v2.0.6 // indirect
-	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/crypto v0.0.0-20220507011949-2cf3adece122 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
-	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/term v0.0.0-20220411215600-e5f449aeb171 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- small-repro/main.go
+++ small-repro/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	util "github.com/dagger/dlsp/small-repro/convertor"
+	"github.com/mitchellh/mapstructure"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 	"github.com/tliron/glsp/server"
 	"github.com/tliron/kutil/logging"
 
 	// Must include a backend implementation. See kutil's logging/ for other options.
+
 	_ "github.com/tliron/kutil/logging/simple"
 )
 
@@ -25,21 +27,24 @@ func main() {
 	logging.Configure(2, nil)
 
 	handler = protocol.Handler{
-		Initialize:            initialize,
-		Initialized:           initialized,
-		Shutdown:              shutdown,
-		SetTrace:              setTrace,
-		TextDocumentDidChange: documentDidChange,
-		TextDocumentDidOpen:   documentDidOpen,
+		Initialize:                      initialize,
+		Initialized:                     initialized,
+		Shutdown:                        shutdown,
+		SetTrace:                        setTrace,
+		TextDocumentDidChange:           documentDidChange,
+		TextDocumentDidOpen:             documentDidOpen,
+		WorkspaceDidChangeConfiguration: didChangeConfiguration,
 	}
 
 	server := server.NewServer(&handler, lsName, false)
 
-	log.Errorf("Run server Stdio")
+	log.Infof("Run server Stdio")
 	server.RunStdio()
 }
 
 func initialize(context *glsp.Context, params *protocol.InitializeParams) (interface{}, error) {
+	log.Infof("Initializing Language Server")
+
 	capabilities := handler.CreateServerCapabilities()
 	change := protocol.TextDocumentSyncKindFull
 	capabilities.TextDocumentSync = protocol.TextDocumentSyncOptions{
@@ -53,10 +58,6 @@ func initialize(context *glsp.Context, params *protocol.InitializeParams) (inter
 			ChangeNotifications: &protocol.BoolOrString{Value: util.BoolPtr(true)},
 		}}
 
-	if params.Trace != nil {
-		protocol.SetTraceValue(*params.Trace)
-	}
-
 	log.Infof("rootPath: %#v", params.WorkspaceFolders)
 
 	return protocol.InitializeResult{
@@ -69,24 +70,49 @@ func initialize(context *glsp.Context, params *protocol.InitializeParams) (inter
 }
 
 func initialized(context *glsp.Context, params *protocol.InitializedParams) error {
-	log.Errorf("Initialized")
-	log.Errorf("params: %#v", params)
+	log.Infof("Initialized")
+	log.Infof("params: %#v", params)
 
-	var result interface{}
-	context.Call("workspace/workspaceFolders", "bar", &result)
-	log.Infof("Result: %#v", result)
+	// var result interface{}
+	// context.Call("workspace/workspaceFolders", "bar", &result)
+	// log.Infof("Result: %#v", result)
 	return nil
 }
 
 func shutdown(context *glsp.Context) error {
-	log.Errorf("Shutdown")
+	log.Infof("Shutdown")
 	protocol.SetTraceValue(protocol.TraceValueOff)
 	return nil
 }
 
 func setTrace(context *glsp.Context, params *protocol.SetTraceParams) error {
-	log.Errorf("Set strace")
+	log.Infof("Set strace: %s mode", params.Value)
 	protocol.SetTraceValue(params.Value)
+	return nil
+}
+
+func didChangeConfiguration(context *glsp.Context, params *protocol.DidChangeConfigurationParams) error {
+	// "dlsp.trace.server": "off" | "message" | "verbose"
+	type Config struct {
+		DLSP struct {
+			Trace struct {
+				Server string
+			}
+		}
+	}
+
+	// Parse the configuration
+	var confs Config
+	err := mapstructure.Decode(params.Settings, &confs)
+	// Set trace level to corresponding option
+	if err != nil {
+		log.Infof("Failed to parse trace level, setting it to off")
+		return err
+	}
+
+	setTrace(context, &protocol.SetTraceParams{
+		Value: protocol.TraceValue(confs.DLSP.Trace.Server),
+	})
 	return nil
 }
 
