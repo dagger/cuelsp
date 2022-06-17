1c78bfe706d89e4048b1df28579fa098a84c5f34
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-06-17 18:14:54 +0200

    doc(dlsp/parser): improve documentation
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- parser/utils.go
+++ parser/utils.go
@@ -4,6 +4,11 @@ import (
 	"strings"
 )
 
+// IsDefinition returns true if the current name is a CUE definition
+// Pattern detected are:
+// - #Foo
+// - _#Foo
+// It returns false if it's not a definition
 func IsDefinition(name string) bool {
 	return strings.HasPrefix(name, "#") || strings.HasPrefix(name, "_#")
 }
